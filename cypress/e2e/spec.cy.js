03119e270ade83ba80c530687a3caf469b5454a8
stanleynnjoroge <stanley.nnjoroge@griffinglobaltech.com>
2024-10-31 14:21:14 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,14 @@
 describe("My First Test", () => {
   beforeEach(() => {
-    cy.visit("http://172.31.48.1:3000");
+    cy.visit("http://10.1.0.193:3000");
     cy.wait(3000);
   });
   it("It routes to my app", () => {
-    cy.url().should("include", "http://172.31.48.1:3000");
+    cy.url().should("include", "http://10.1.0.193:3000");
   });
   it("Navigates from Dashboard to Register Admin Page", () => {
     cy.contains("Dashboard").click();
-    cy.url().should("include", "http://172.31.48.1:3000/Dashboard");
+    cy.url().should("include", "http://10.1.0.193:3000/Dashboard");
     cy.get(".ant-menu-item").contains("Register Admin").click();
     cy.url().should("include", "/register-admin");
   });
